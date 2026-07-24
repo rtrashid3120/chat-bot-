b4e9fd8282d8f6a03005fe4c77da5d84842691ca
Mohamed Rashid <mohamedrashid@Mohameds-MacBook-Air.local>
2026-07-25 02:16:57 +0530

    fix: mock AUTH_URL during vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
+if (process.env.NODE_ENV === "production") {
+  if (!process.env.AUTH_URL) process.env.AUTH_URL = "https://placeholder-url.vercel.app";
+  if (!process.env.NEXTAUTH_URL) process.env.NEXTAUTH_URL = "https://placeholder-url.vercel.app";
+}
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
